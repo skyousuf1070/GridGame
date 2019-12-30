6cd1794f9bdcb3802b8283419275c3862e74e9f1
skyousuf1070 <skyousuf1070@gmail.com>
2019-12-30 10:23:51 +0530

    add files to GridGame

---
+++ src/Position.java
@@ -0,0 +1,16 @@
+public class Position {
+    private int x_coOrdinate, y_coOrdinate;
+
+    public Position(int x_coOrdinate, int y_coOrdinate) {
+        this.x_coOrdinate = x_coOrdinate;
+        this.y_coOrdinate = y_coOrdinate;
+    }
+
+    int getX_coOrdinate() {
+        return x_coOrdinate;
+    }
+
+    int getY_coOrdinate() {
+        return y_coOrdinate;
+    }
+}
